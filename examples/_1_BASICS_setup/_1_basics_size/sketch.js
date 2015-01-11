a0e3788dcc8ef89e17ceca19762d3fbe504f4f2a
jguess <lists@guess.fr>
2015-01-11 07:12:18 +0100

    Added basic example files

---
+++ examples/_1_BASICS_setup/_1_basics_size/sketch.js
@@ -0,0 +1,12 @@
+function setup() {
+	createCanvas(windowWidth, windowHeight);
+
+	println("hello");
+
+	println(windowWidth, windowHeight);
+	println(displayWidth, displayHeight);
+	println(width, height);
+
+	background(255, 0, 0);
+	point(100,100);
+}
